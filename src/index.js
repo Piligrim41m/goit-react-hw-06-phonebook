d9a3cf3ac644bfd2bf1a29af0f2af3c807f025fb
Piligrim41m <lightsun41m@gmail.com>
2023-01-21 18:11:04 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -10,13 +9,13 @@ import { PersistGate } from 'redux-persist/integration/react';
 // ReactDOM.createRoot(document.getElementById('root')).render(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Provider store={store}>
         <App />
-      </Provider> 
-    </PersistGate>
-  </React.StrictMode>
+    </PersistGate >
+  </Provider> 
+  // </React.StrictMode>
 );
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
